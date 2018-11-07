6a057c5d21b4b3a0d1a3a65b3a17189dce5040ee
guohaiqiangwork <guohaiqiangwork@outlook.com>
2018-11-07 19:37:05 +0800

    iview 样式引入

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iView';
+import 'iview/dist/styles/iview.css'
+import locale from 'iview/dist/locale/en-US';
 
 Vue.config.productionTip = false
-
+Vue.use(iView, { locale });
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
